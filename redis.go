11d8d35f40eeea68a5e22769d0978b05560f2347
zhangmeng-pd <zhangmeng-pd@test219x.ops.corp.qihoo.net>
2013-08-02 14:17:40 +0800

    delete repeat method

--- redis.go
+++ redis.go
@@ -932,37 +932,6 @@ func (client *Client) Zmadd(key string, values map[string]float64 ) (bool, error
 	return res.(int64) >= 1, nil
 }
 
-// sorted multi set commands, with redis >=2.4
-
-func (client *Client) Zmadd(key string, values map[string]float64 ) (bool, error) {
-      args := []string{key}
-      for value,score := range values {
-          args = append(args, strconv.FormatFloat(score, 'f', -1, 64))
-          args = append(args, value)
-      }
-      res, err := client.sendCommand("ZADD", args...)
-      if err != nil {
-          return false, err
-      }
-  
-      return res.(int64) >= 1, nil
-}
-
-
-func (client *Client) Zmadd(key string, values map[string]float64 ) (bool, error) {
-      args := []string{key}
-      for value,score := range values {
-          args = append(args, strconv.FormatFloat(score, 'f', -1, 64))
-          args = append(args, value)
-      }
-      res, err := client.sendCommand("ZADD", args...)
-      if err != nil {
-          return false, err
-      }
-  
-      return res.(int64) >= 1, nil
-}
-  
 func (client *Client) Zrem(key string, value []byte) (bool, error) {
 	res, err := client.sendCommand("ZREM", key, string(value))
 	if err != nil {
@@ -1005,12 +974,12 @@ func (client *Client) Zrange(key string, start int, end int,WITHSCORES ...string
       args := []string{key,strconv.Itoa(start), strconv.Itoa(end)}
       if len(WITHSCORES) == 1 && WITHSCORES[0] == "WITHSCORES" {
           args = append(args,"WITHSCORES")
-      }    
+      }
       res, err := client.sendCommand("ZRANGE", args...)
       if err != nil {
           return nil, err
-      }    
-  
+      }
+
       return res.([][]byte), nil
 }
 
